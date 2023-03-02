345e6e4ef69240e3233738ea1aa3b4057c00f2a5
Thiccblique <104588992+Thiccblique@users.noreply.github.com>
2023-03-02 11:19:19 -0800

    Fixed the bow
    I also migh have scred the animator

--- Dirt_TheGame/Assets/Scripts/PlayerScripts/PlayerScript.cs
+++ Dirt_TheGame/Assets/Scripts/PlayerScripts/PlayerScript.cs
@@ -81,12 +81,19 @@ public class PlayerScript : MonoBehaviour
         {
             Launch();
         }
-       
+
+        Vector2 move = new Vector2(movement.x, movement.y);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
        
     }
     void Launch()
     {
-        GameObject projectileObject = Instantiate(projectilePrefab, rb.position + Vector2.up * 0.5f, Quaternion.identity);
+        GameObject projectileObject = Instantiate(projectilePrefab, rb.position, Quaternion.identity);
 
         Projectile projectile = projectileObject.GetComponent<Projectile>();
         projectile.Launch(lookDirection, 300);
--- Dirt_TheGame/Assets/Scripts/Projectile.cs
+++ Dirt_TheGame/Assets/Scripts/Projectile.cs
@@ -27,7 +27,7 @@ public class Projectile : MonoBehaviour
 
     void Update()
     {
-        if (transform.position.magnitude > 1000.0f)
+        if (transform.position.magnitude > 100.0f)
         {
             Destroy(gameObject);
         }
